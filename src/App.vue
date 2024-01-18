c52dba6422610457d5378f4f7c930b6963e3d3df
Matej7418 <58819158+Matej7418@users.noreply.github.com>
2024-01-18 21:46:41 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <h1>Kalkulacka</h1>
+        <div v-for=""></div>
+    </main>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+let num1 = 0;
+let num2 = 0;
+let op = '';
+</script>
+
+<style scoped>
+main {
+}
+
+</style>
